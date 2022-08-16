c246564e43710af3cc9a715cc08ca166cb74b174
Fran Linde Blazquez <franlindebl@gmail.com>
2022-08-16 19:30:44 +0200

    Added sample with effects to add numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // Estados, variables... 
+  const [firstNumber, setFirstNumber] = React.useState(0);
+  const [secondNumber, setSecondNumber] = React.useState(0);
+  const [result, setResult] = React.useState(0);
+
+  // Efectos
+
+  // Este efecto se ejecuta en cada repintado (no tiene lista de dependencias, React no sabe cuales son)
+  React.useEffect(() => {
+    console.log("Efecto que se ejecuta en repintado!!");
+  });
+
+  // Este efecto se ejecuta solo en el primer pintado (le decimos a React que no tiene dependencias -> [])
+  React.useEffect(() => {
+    console.log("Efecto que solo se ejecuta en el inicio!!");
+  }, []);
+
+  // Este efecto se va a ejecutar cuando cambie la variable firstnumber o secondNumber
+  React.useEffect(() => {
+    console.log("Efecto que solo se ejecuta si cambia firstNumber o secondNumber!!");
+    setResult(firstNumber + secondNumber);
+  }, [firstNumber, secondNumber]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" value={firstNumber} onChange={(event) => {
+        setFirstNumber(parseInt(event.target.value));
+      }} />
+      <input type="number" value={secondNumber} onChange={(event) => {
+        setSecondNumber(parseInt(event.target.value));
+      }} />
+      <p>El resultado de la suma es: {result}</p>
     </div>
   );
 }
