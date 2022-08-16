709ba98e3a35eb8b0499a3c4608a643896df52c8
Fran Linde Blazquez <franlindebl@gmail.com>
2022-08-16 19:51:37 +0200

    Added return functions on effects

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ function App() {
 
   // Efectos
 
-  // Este efecto se ejecuta en cada repintado (no tiene lista de dependencias, React no sabe cuales son)
+  // Este efecto se ejecuta en cada repintado (porque React no tiene lista de dependencias, no sabe cuales son)
   React.useEffect(() => {
-    console.log("Efecto que se ejecuta en repintado!!");
+    console.log("INICIO 1: Efecto que se ejecuta en repintado!!");
+
+    // La función del return se ejecuta ANTES de cada repintado
+    return () => console.log("FINAL 1: Efecto que se ejecuta en repintado!!");
   });
 
-  // Este efecto se ejecuta solo en el primer pintado (le decimos a React que no tiene dependencias -> [])
+  // Este efecto se ejecuta solo en el primer pintado (porque le decimos a React que no tiene dependencias -> [])
   React.useEffect(() => {
-    console.log("Efecto que solo se ejecuta en el inicio!!");
+    console.log("INICIO 2: Efecto que solo se ejecuta en el inicio!!");
+
+    // La función del return se ejecuta en la muerte del componente
+    return () => console.log("FINAL 2: Efecto que solo se ejecuta en el inicio!!");
   }, []);
 
   // Este efecto se va a ejecutar cuando cambie la variable firstnumber o secondNumber
   React.useEffect(() => {
-    console.log("Efecto que solo se ejecuta si cambia firstNumber o secondNumber!!");
+    console.log("INICIO 3: Efecto que solo se ejecuta si cambia firstNumber o secondNumber!!");
     setResult(firstNumber + secondNumber);
+    
+    // La función del return se ejecuta previa ejecución del propio efecto
+    return () => console.log("FINAL 3: Efecto que solo se ejecuta si cambia firstNumber o secondNumber!!");
   }, [firstNumber, secondNumber]);
 
   return (
